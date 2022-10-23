7890aa8e25eb9404d2ac524e909a891b7028cdb5
troubleskiller <troubleskiller@outlook.com>
2022-10-23 14:44:55 +0800

    add image and do some flex layout

--- lib/screen/main_screen/main_screen.dart
+++ lib/screen/main_screen/main_screen.dart
@@ -16,11 +16,50 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      height: 100,
-      width: 100,
-      color: Colors.green,
-    ));
+    return Flex(
+      direction: Axis.horizontal,
+      children: [
+        Expanded(
+          flex: 2,
+          child: Container(
+            // width: 560,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hi, I\'m Troubleskiller',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'I\'m a software engineer from china,who creates open-source projects and writes about code, design, and life.',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            // margin: EdgeInsets.only(right: ),
+            // height: 400,
+            // width: 300,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                  image: AssetImage('assets/image/main_page_image.jpg'),
+                  fit: BoxFit.cover),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
